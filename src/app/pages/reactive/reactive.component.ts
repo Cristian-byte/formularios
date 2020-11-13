ec47e4e28c97a99bfd012b6a5a7b57de79cd772b
Cristian-byte <andygign40@gmail.com>
2020-11-13 00:40:23 -0600

    Reactivo Creacion del formulario

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+  constructor( private fb: FormBuilder ) {
+    this.crearFormulario();
+   }
 
   ngOnInit(): void {
+  }
+
+  crearFormulario() {
+    this.forma = this.fb.group({
+      nombre  : ['Cristian'],
+      apellido: ['Sanchez'],
+      correo  : ['dsadsa@dasdas.com'],
+    });
+  }
+
+  guardar() {
+    console.log( this.forma );
   }
 
 }
