43ef81703993c8568cb8dc6af7f050cca332a2f3
Cristian-byte <andygign40@gmail.com>
2020-11-13 15:52:39 -0600

    Reactivo Validaciones y HTML

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -17,16 +17,34 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched
+  }
+  get emailNoValido() {
+    return this.forma.get('email').invalid && this.forma.get('email').touched
+  }
+
   crearFormulario() {
     this.forma = this.fb.group({
       nombre  : ['', [ Validators.required, Validators.minLength(5) ] ],
-      apellido: ['', Validators.required ],
-      correo  : ['', [ Validators.required, Validators.pattern('[a - z0 -9._ % +-] +@[a - z0 - 9. -]+\.[a - z]{ 2, 3 } $]')] ],
+      apellido: ['',   Validators.required],
+      email: ['', [Validators.required, Validators.email ] ],
     });
   }
 
   guardar() {
     console.log( this.forma );
+
+    if (this.forma.invalid) {
+
+      return Object.values(this.forma.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
   }
 
 }
