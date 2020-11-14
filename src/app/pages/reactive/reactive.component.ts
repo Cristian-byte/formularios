75e495569bc1f9c05690a22af958c5b75b6c8602
Cristian-byte <andygign40@gmail.com>
2020-11-13 20:06:43 -0600

    Reactivo Validaciones Personalizadas

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { get } from 'http';
+import { ValidadoresService } from '../../services/validadores.service';
+
 
 @Component({
   selector: 'app-reactive',
@@ -11,7 +13,8 @@ export class ReactiveComponent implements OnInit {
 
   forma: FormGroup;
 
-  constructor( private fb: FormBuilder ) {
+  constructor( private fb: FormBuilder,
+               private validadores: ValidadoresService ) {
     this.crearFormulario();
     this.cargarDataAlFormulario();
    }
@@ -30,26 +33,44 @@ export class ReactiveComponent implements OnInit {
   get apellidoNoValido() {
     return this.forma.get('apellido').invalid && this.forma.get('apellido').touched
   }
+
   get emailNoValido() {
     return this.forma.get('email').invalid && this.forma.get('email').touched
   }
+
   get estadoNoValido() {
     return this.forma.get('direccion.estado').invalid && this.forma.get('direccion.estado').touched
   }
+
   get municipioNoValido() {
     return this.forma.get('direccion.municipio').invalid && this.forma.get('direccion.municipio').touched
+  }
+
+  get pass1NoValido() {
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched;
+  }
+
+  get pass2NoValido() {
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+
+    return ( pass1 === pass2 ) ? false : true;
   }
 
   crearFormulario() {
     this.forma = this.fb.group({
       nombre   : ['', [ Validators.required, Validators.minLength(4) ] ],
-      apellido : ['',   Validators.required],
-      email    : ['', [Validators.required, Validators.email ] ],
+      apellido : ['', [ Validators.required, this.validadores.noHerrera ] ],
+      email    : ['', [ Validators.required, Validators.email ] ],
+      pass1    : ['',   Validators.required ],
+      pass2    : ['',   Validators.required ],
       direccion: this.fb.group({
         estado   : ['', Validators.required],
         municipio: ['', Validators.required],
       }),
       pasatiempos: this.fb.array([])
+    },{
+      
     });
   }
 
